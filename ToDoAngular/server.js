087e4905ed039ff3fa3464153e40d879ecec6087
lokesh <yell.nagalokesh@gmail.com>
2018-03-06 21:11:19 +0530

    initial commit

--- ToDoAngular/server.js
+++ ToDoAngular/server.js
@@ -5,6 +5,7 @@
     var app      = express();                        // create our app w/ express
     var mongoose = require('mongoose');              // mongoose for mongodb
     var morgan   = require('morgan');                // log requests to the console (express4)
+    var VRNHeader = require('./app/models/VRNHeaderModel');
     var bodyParser = require('body-parser');         // pull information from HTML POST (express4)
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
     var database = require('./config/database');
@@ -14,7 +15,7 @@
     // configuration ===============================================================
     mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(express.static(__dirname + '/public/src'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
@@ -22,7 +23,7 @@
     app.use(methodOverride());
 
     // routes ======================================================================
-    require('./app/routes.js')(app);
+    require('./app/routes/VRNHeaderRoutes.js')(app);
 
     // listen (start app with node server.js) ======================================
     app.listen(port);
